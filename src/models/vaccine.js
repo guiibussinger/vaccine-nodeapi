0a0106c0291453694652291a02fd498ed08a37d0
Guilherme Pedro <guilhermepedro@ioasys.com.br>
2019-11-28 02:27:34 -0300

    integrating with api

--- src/models/vaccine.js
+++ src/models/vaccine.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const VaccineSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  date: { type: Date, required: true }
+  date: { type: Date, required: true },
+  dosage: { type: String, required: true }
 });
 
 mongoose.model('Vaccine', VaccineSchema);
